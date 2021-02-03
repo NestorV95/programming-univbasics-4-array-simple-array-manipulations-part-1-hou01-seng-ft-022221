0389f362c449f31dac1910dbf1ba56ead91288c5
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 19:19:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-   = "violet"
-   = array.push element
+  next_color = "violet"
+ updated_array  = colors_in_the_rainbow.push next_color
 end
