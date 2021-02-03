ece8334bcda0f791817e6a61e4e9dd76b5ebbd91
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 19:16:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def using_push(array, element)
   array = ["red", "orange", "yellow", "green", "blue", "indigo"]
   element = "violet"
-  updated_array = using_push
+  updated_array = array.push element
 end
