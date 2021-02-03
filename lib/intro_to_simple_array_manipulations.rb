e317b1e9cd62a2f32a4ebcfeb1650322df055e9f
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 20:09:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,5 +13,4 @@ end
 def using_pop(continents)
 continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
 updated_array = continents.pop
-continents.size
 end
