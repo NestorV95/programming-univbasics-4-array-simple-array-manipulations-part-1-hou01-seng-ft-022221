1f3fe908fa7576320a7212ae31a49a98dc37b19c
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-03 20:00:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,6 +12,6 @@ end
 
 def using_pop(continents)
 continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-continents.pop
-deleted_string = "Antarctica"
+new_array =  continents.pop
+return continents
 end
